67e482a781211ea0131ac1b7961e5e8d76a815cb
phuonghoangg <hoangphuong291199@gmail.com>
2023-08-02 21:57:50 +0700

    employee

---
+++ ManagermenEmployeesGroupEight/src/main/java/com/example/managermenemployeesgroupeight/ManagermenEmployeesGroupEightApplication.java
@@ -0,0 +1,13 @@
+package com.example.managermenemployeesgroupeight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagermenEmployeesGroupEightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagermenEmployeesGroupEightApplication.class, args);
+	}
+
+}
---
+++ ManagermenEmployeesGroupEight/src/test/java/com/example/managermenemployeesgroupeight/ManagermenEmployeesGroupEightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managermenemployeesgroupeight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagermenEmployeesGroupEightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
